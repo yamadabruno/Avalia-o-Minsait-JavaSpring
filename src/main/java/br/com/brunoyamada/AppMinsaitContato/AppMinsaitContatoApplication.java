93b86db59cb98791415d52b3b07d0ddee9b32679
yamadabruno <bruno_yamada@hotmail.com>
2023-11-01 20:15:21 -0300

    first commit

---
+++ src/main/java/br/com/brunoyamada/AppMinsaitContato/AppMinsaitContatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunoyamada.AppMinsaitContato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMinsaitContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMinsaitContatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunoyamada/AppMinsaitContato/AppMinsaitContatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoyamada.AppMinsaitContato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMinsaitContatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
