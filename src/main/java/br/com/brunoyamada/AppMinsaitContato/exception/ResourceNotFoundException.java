e7481887b9084b57e5936f844617bb2b6536cce7
yamadabruno <bruno_yamada@hotmail.com>
2023-11-01 22:07:10 -0300

    exception / config

---
+++ src/main/java/br/com/brunoyamada/AppMinsaitContato/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.brunoyamada.AppMinsaitContato.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
